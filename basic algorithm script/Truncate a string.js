dd1f06393711ca65e3ca7336cba847a9ec98962b
Moredou <simonliuwk@gmail.com>
2017-11-18 19:25:22 -0500

    update

---
+++ basic algorithm script/Truncate a string.js
@@ -0,0 +1,22 @@
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+// Note that inserting the three dots to the end will add to the string length.
+
+// However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.
+
+
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if(num>=str.length){
+      return str;
+  }
+  else if(num<=3){
+      return str.slice(0,num)+'...';
+  }
+  else{
+    return str.slice(0,num-3)+'...';
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
